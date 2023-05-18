94853a33acb72f009003e6d550ae439fdc605810
Michael Adeshina <adeshinaobafemi09@gmail.com>
2023-05-18 23:48:01 +0100

    feat: projectData.js added to the app

---
+++ src/components/projectData/projectData.js
@@ -0,0 +1,62 @@
+export const projectData = [
+  {
+    id: 1,
+    music: "Unavailable",
+    singer: "Davido",
+  },
+  {
+    id: 2,
+    music: "Unavailable",
+    singer: "Davido",
+  },
+  {
+    id: 3,
+    music: "Unavailable",
+    singer: "Davido",
+  },
+  {
+    id: 4,
+    music: "Unavailable",
+    singer: "Davido",
+  },
+  {
+    id: 5,
+    music: "Unavailable",
+    singer: "Davido",
+  },
+  {
+    id: 6,
+    music: "Unavailable",
+    singer: "Davido",
+  },
+  {
+    id: 7,
+    music: "Unavailable",
+    singer: "Davido",
+  },
+  {
+    id: 8,
+    music: "Unavailable",
+    singer: "Davido",
+  },
+  {
+    id: 9,
+    music: "Unavailable",
+    singer: "Davido",
+  },
+  {
+    id: 10,
+    music: "Unavailable",
+    singer: "Davido",
+  },
+  {
+    id: 11,
+    music: "Unavailable",
+    singer: "Davido",
+  },
+  {
+    id: 12,
+    music: "Unavailable",
+    singer: "Davido",
+  },
+];
