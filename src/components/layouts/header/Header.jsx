8a50c968a3bc0f9ac8e03c5d04001490ada13737
Michael Adeshina <adeshinaobafemi09@gmail.com>
2023-05-18 23:03:18 +0100

    feat&style: Header contents added and its styling done

--- src/components/layouts/header/Header.jsx
+++ src/components/layouts/header/Header.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
 import './Header.css'
+import { Link } from 'react-router-dom'
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='headerWrapper'>
+      <div className='headerContents'>
+         <ul>
+            <li className='header-link_list'>
+               <Link to="#"  className='header-link' id="header-link_contact">Contact us</Link>
+            </li>
+            <li className='header-link_list'>
+               <Link to="#"  className='header-link' id="header-link_sign">Sign up</Link>
+            </li>
+         </ul>
+      </div>
+    </div>
   )
 }
 
