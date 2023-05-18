3fab6c2dac0bf968fbfd8532e8344fc5e98e76e2
Michael Adeshina <adeshinaobafemi09@gmail.com>
2023-05-18 23:36:43 +0100

    feat&style: background added to the sidebar and it postion set to fixed

--- src/components/layouts/sidebar/Sidebar.jsx
+++ src/components/layouts/sidebar/Sidebar.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
-import './Sidebar.css'
+import React from "react";
+import "./Sidebar.css";
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
-  )
-}
-
-export default Sidebar
+    <div className="sidebarWrapper">
+      <div className="sidebarContents">Sidebar</div>
+    </div>
+  );
+};
+export default Sidebar;
