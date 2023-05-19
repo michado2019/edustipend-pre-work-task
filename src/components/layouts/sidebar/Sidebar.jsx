0cdcef499d7571249dc76ca0fb5a4eb471031852
Michael Adeshina <adeshinaobafemi09@gmail.com>
2023-05-19 04:50:40 +0100

    feat & style: Sidebar.jsx content added and styled

--- src/components/layouts/sidebar/Sidebar.jsx
+++ src/components/layouts/sidebar/Sidebar.jsx
@@ -1,9 +1,23 @@
 import React from "react";
 import "./Sidebar.css";
+import { projectData } from "../../projectData/projectData";
+import { Add } from "@mui/icons-material";
 const Sidebar = () => {
   return (
     <div className="sidebarWrapper">
-      <div className="sidebarContents">Sidebar</div>
+      <div className="sidebarContents">
+          {
+            projectData.slice(0, 5).map((each) => {
+              return(
+                <div className="sidebarContent" key={each.id}>
+                 <p className="sidebarMusic">{each.music} by {each.singer}</p>
+                 <Add className="sidebarAdd-icon" />
+                </div>
+              )
+            })
+          }
+          <button className="sidebarBtn">View more</button>
+      </div>
     </div>
   );
 };
