d3b4c7186407b0bf7dc62f98035edae74df9a814
Michael Adeshina <adeshinaobafemi09@gmail.com>
2023-05-19 07:48:46 +0100

    feat: contents added to the homepage

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 import './Home.css'
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className="homeWrapper">
+        <div className="homeContents">
+           <h2 className="homeTitle">Make your party fun!</h2>
+           <p className="homeDetails">Create your own custom playlist today.</p>
+           <button className="homeBtn">Create playlist</button>
+        </div>
+    </div>
   )
 }
-
 export default Home
